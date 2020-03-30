a63dd2b511585c0034116ac81ce37fb9d98f36f5
possible3656 <sj94611@gmail.com>
2020-03-30 16:53:11 +0530

    added score and high score using shared preference

---
+++ app/src/main/java/com/pscube/trivia/data/asyncanswer.java
@@ -0,0 +1,12 @@
+package com.pscube.trivia.data;
+
+import com.pscube.trivia.models.questions;
+
+import java.util.ArrayList;
+
+public interface asyncanswer {
+
+
+
+    void processFinished (ArrayList<questions> questionsArrayList);
+}
